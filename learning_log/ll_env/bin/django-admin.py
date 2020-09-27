0c7cf8549e3e63f6c3a57e0a81a080568614969c
gwiily <gwiily@qq.com>
2020-09-28 00:57:18 +0800

    update learning_log

---
+++ learning_log/ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/gwiily/Downloads/python编程：从入门到实践/learning_log/ll_env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
